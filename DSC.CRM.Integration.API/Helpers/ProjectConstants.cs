5ef3914ce10e567b86823dfcebb7c6222ac299da
CRMDSC <crm@dubaisc.ae>
2026-03-13 18:29:09 +0800

    Done CHnages

---
+++ DSC.CRM.Integration.API/Helpers/ProjectConstants.cs
@@ -0,0 +1,63 @@
+﻿namespace DSC.CRM.Integration.API.Helpers
+{
+    public static class ProjectConstants
+    {
+        // ===============================
+        // Gender
+        // ===============================
+        public static class Gender
+        {
+            public const int Male = 1;
+            public const int Female = 2;
+            public const int Both = 3;
+            public const int Kids = 4;
+        }
+
+        // ===============================
+        // Establishment Type
+        // ===============================
+        public static class EstablishmentType
+        {
+            public const int SportServiceCompany = 1;
+            public const int PrivateSportsClub = 2;
+            public const int SportsCompany = 3;
+            public const int SportsAcademy = 4;
+            public const int FitnessCenter = 5;
+            public const int ESport = 6;
+            public const int EventOrganizer = 7;
+            public const int PrivateCompany = 8;
+            public const int PublicCompany = 9;
+            public const int School = 10;
+            public const int University = 11;
+        }
+
+        // ===============================
+        // License Source
+        // ===============================
+        public static class LicenseSource
+        {
+            public const int DET = 1;
+            public const int SharjahFreezone = 2;
+            public const int DubaiFreezone = 3;
+            public const int InitialApproval = 4;
+            public const int Default = 0;
+        }
+
+        // ===============================
+        // Request Status
+        // ===============================
+        public static class EServiceRequestStatus
+        {
+            public const int Approved = 1;
+            public const int Received = 2;
+            public const int Rejected = 3;
+            public const int MissingDocuments = 4;
+            public const int ApprovedBySubAdmin = 5;
+            public const int Processing = 6;
+            public const int Cancelled = 7;
+            public const int ConditionallyApproved = 8;
+            public const int Unknown = 0;
+
+        }
+    }
+}
--- DSC.CRM.Integration.API/Services/EstablishmentService.cs
+++ DSC.CRM.Integration.API/Services/EstablishmentService.cs
@@ -17,7 +17,7 @@ namespace DSC.CRM.Integration.API.Services
         public Guid CreateEstablishment(EstablishmentRequest request)
         {
             var service = _crmConnector.GetCRMService();
-
+            //
             // ===============================
             // Create Establishment Request
             // ===============================
@@ -42,17 +42,17 @@ namespace DSC.CRM.Integration.API.Services
 
             establishment["dsc_establishmentphoto"] = request.EstablishmentPhoto;
 
-            //// Latitude
-            //if (!string.IsNullOrEmpty(request.Lat))
-            //{
-            //    establishment["dsc_latitude"] = request.Lat;
-            //}
+            // Latitude
+            if (request.Lat != 0)
+            {
+                establishment["dsc_latitude"] = request.Lat.ToString();
+            }
 
-            //// Longitude
-            //if (!string.IsNullOrEmpty(request.Lng))
-            //{
-            //    establishment["dsc_longitude"] = request.Lng;
-            //}
+            // Longitude
+            if (request.Lng != 0)
+            {
+                establishment["dsc_longitude"] = request.Lng.ToString();
+            }
 
             //// Country ID (TEXT)
             //if (!string.IsNullOrEmpty(request.CountryID))
@@ -65,10 +65,150 @@ namespace DSC.CRM.Integration.API.Services
             //{
             //    establishment["dsc_totalstaff"] = request.TotalStaff.Value;
             //}
+            if (request.TotalStaff > 0)
+            {
+                establishment["dsc_totalstaff"] = request.TotalStaff;
+            }
+            if (request.CountryID > 0)
+            {
+                establishment["dsc_countryid"] = request.CountryID.ToString();
+            }
 
             // Private Contact
             establishment["dsc_privateemail"] = request.EmailPrivate;
             establishment["dsc_privatephone"] = request.PhonePrivate;
+
+            //// Gender Mapping
+            //if (request.Gender == ProjectConstants.Gender.Male)
+            //{
+            //    establishment["dsc_gender"] = ProjectConstants.Gender.Male;
+            //}
+            //else if (request.Gender == ProjectConstants.Gender.Female)
+            //{
+            //    establishment["dsc_gender"] = ProjectConstants.Gender.Female;
+            //}
+            //else if (request.Gender == ProjectConstants.Gender.Both)
+            //{
+            //    establishment["dsc_gender"] = ProjectConstants.Gender.Both;
+            //}
+            //else if (request.Gender == ProjectConstants.Gender.Kids)
+            //{
+            //    establishment["dsc_gender"] = ProjectConstants.Gender.Kids;
+            //}
+            //// EStablishment Type
+            switch (request.EstablishmentType)
+            {
+                case ProjectConstants.EstablishmentType.SportServiceCompany:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.SportServiceCompany);
+                    break;
+
+                case ProjectConstants.EstablishmentType.PrivateSportsClub:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.PrivateSportsClub);
+                    break;
+
+                case ProjectConstants.EstablishmentType.SportsCompany:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.SportsCompany);
+                    break;
+
+                case ProjectConstants.EstablishmentType.SportsAcademy:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.SportsAcademy);
+                    break;
+
+                case ProjectConstants.EstablishmentType.FitnessCenter:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.FitnessCenter);
+                    break;
+
+                case ProjectConstants.EstablishmentType.ESport:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.ESport);
+                    break;
+
+                case ProjectConstants.EstablishmentType.EventOrganizer:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.EventOrganizer);
+                    break;
+
+                case ProjectConstants.EstablishmentType.PrivateCompany:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.PrivateCompany);
+                    break;
+
+                case ProjectConstants.EstablishmentType.PublicCompany:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.PublicCompany);
+                    break;
+
+                case ProjectConstants.EstablishmentType.School:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.School);
+                    break;
+
+                case ProjectConstants.EstablishmentType.University:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.University);
+                    break;
+
+                default:
+                    establishment["dsc_establishmenttype"] = new OptionSetValue(ProjectConstants.EstablishmentType.SportServiceCompany);
+                    break;
+            }
+
+            //switch (request.Status)
+            //{
+            //    case ProjectConstants.EServiceRequestStatus.Approved:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Approved;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.Received:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Received;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.Rejected:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Rejected;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.MissingDocuments:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.MissingDocuments;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.ApprovedBySubAdmin:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.ApprovedBySubAdmin;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.Processing:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Processing;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.Cancelled:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Cancelled;
+            //        break;
+
+            //    case ProjectConstants.EServiceRequestStatus.ConditionallyApproved:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.ConditionallyApproved;
+            //        break;
+
+            //    default:
+            //        establishment["dsc_status"] = ProjectConstants.EServiceRequestStatus.Unknown;
+            //        break;
+            //}
+
+            switch (request.LicenseSourceID)
+            {
+                case ProjectConstants.LicenseSource.DET:
+                    establishment["dsc_licensesource"] = new OptionSetValue(ProjectConstants.LicenseSource.DET);
+                    break;
+
+                case ProjectConstants.LicenseSource.SharjahFreezone:
+                    establishment["dsc_licensesource"] = new OptionSetValue(ProjectConstants.LicenseSource.SharjahFreezone);
+                    break;
+
+                case ProjectConstants.LicenseSource.DubaiFreezone:
+                    establishment["dsc_licensesource"] = new OptionSetValue(ProjectConstants.LicenseSource.DubaiFreezone);
+                    break;
+
+                case ProjectConstants.LicenseSource.InitialApproval:
+                    establishment["dsc_licensesource"] = new OptionSetValue(ProjectConstants.LicenseSource.InitialApproval);
+                    break;
+
+                default:
+                    establishment["dsc_licensesource"] = new OptionSetValue(ProjectConstants.LicenseSource.Default);
+                    break;
+            }
+
 
             // Tags
             establishment["dsc_tags"] = request.Tags;
